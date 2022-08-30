cc12ab193ae05a040b91eadda4042f19a84f03cf
AE5r3dads <105577795+AE5r3dads@users.noreply.github.com>
2022-08-31 01:24:56 +0200

    Add files via upload

---
+++ LapTop/HP Pavilion 15-dk1026ne Gaming laptop - 10th Intel Core/script.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.img2');
+let container2 = document.querySelector('.container');
+function phones(phone){
+    img.src = phone;
+}
+function colors(color2){
+    container2.style.background = color;
+}
